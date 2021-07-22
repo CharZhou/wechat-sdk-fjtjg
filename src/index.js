488184cba951e717057812ec80c93af049cbe051
CharZhou <zhouzewei@fjtjg.com>
2021-07-22 13:32:00 +0800

    add babel compile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import MiniProgram from './miniprogram';
 import OfficialAccount from './officialaccount';
+import DefaultAccessTokenController from './accesstoken/default';
+import HttpAccessTokenController from './accesstoken/http';
+import IAccessTokenController from './common/accesstoken';
 
 export {
   MiniProgram,
   OfficialAccount,
+  DefaultAccessTokenController,
+  HttpAccessTokenController,
+  IAccessTokenController,
 };
